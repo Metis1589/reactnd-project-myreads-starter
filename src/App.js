5c556eba41b39d2b990962682735c60bf715a8f1
Dmitry Morozov <Morosovdmitry@gmail.com>
2017-10-05 00:03:10 +0300

    Implementing books filtering in list by shelf type.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import BooksList from './BooksList'
 import './App.css'
 
 class BooksApp extends React.Component {
-
     state = {
         books: []
     }
-
+    moveBook = (book, status) => {
+        this.setState((state) => ({
+            books: state.books.map((b) => {if(b.id===book.id){b.shelf = status}; return b})
+        }));
+    }
     componentDidMount() {
         BooksAPI.getAll().then((books) => {
             this.setState({ books })
         })
     }
-
     render() {
         return (
             <div className="app">
                 <Route path='/' exact render={() => (
-                    <BooksList books={this.state.books} />
+                    <BooksList books={this.state.books} onMoveBook={this.moveBook}/>
                 )}/>
                 <Route path='/search' render={({history}) => (
                     <Search/>
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import BooksShelf from './BooksShelf'
 
 class BooksList extends Component {
-
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        onMoveBook: PropTypes.func.isRequired
+    }
     render() {
-
-        const { title, type, books } = this.props
-
-        let showingBooks
-        showingBooks = books
-
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -18,35 +16,9 @@ class BooksList extends Component {
                 </div>
                 <div className="list-books-content">
                     <div>
-                        <div className="bookshelf-books">
-                            <ol className="books-grid">
-                                {showingBooks.map((book) => (
-                                    <li key={book.id}>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select>
-                                                        <option value="none" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">To Kill a Mockingbird</div>
-                                            <div className="book-authors">Harper Lee</div>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ol>
-                        </div>
-                        /*
-                        <BooksShelf title="Currently Reading" type="currentlyReading" books="{this.props.books}"/>
-                        <BooksShelf title="Want to Read" type="wantToRead" books="{this.props.books}"/>
-                        <BooksShelf title="Read" type="read" books="{this.props.books}"/>
-                        */
+                        <BooksShelf title="Currently Reading" type="currentlyReading" books={this.props.books} onMoveBook={this.props.onMoveBook} />
+                        <BooksShelf title="Want to Read" type="wantToRead" books={this.props.books} onMoveBook={this.props.onMoveBook} />
+                        <BooksShelf title="Read" type="read" books={this.props.books} onMoveBook={this.props.onMoveBook} />
                     </div>
                 </div>
                 <div className="open-search">
